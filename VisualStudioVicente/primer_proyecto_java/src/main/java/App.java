958f193ea7d8731f07e21a31285c4624f5b18c3c
MarcosFerrandiz <marcos149ft@gmail.com>
2024-12-02 17:55:33 +0100

    Cambiar por el de

--- VisualStudioVicente/primer_proyecto_java/src/main/java/App.java
+++ VisualStudioVicente/primer_proyecto_java/src/main/java/App.java
@@ -7,7 +7,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int option;
         do {
-            System.out.println("Calculadora de Área by Marcos")
+            System.out.println("Calculadora de Área de Marcos")
             System.out.println("Seleccione una opción")
             System.out.println("1. Círculo");
             System.out.println("2. Cuadrado");
